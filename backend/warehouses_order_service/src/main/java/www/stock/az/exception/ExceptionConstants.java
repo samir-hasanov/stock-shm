e56feb75249dfa66216f6001a35ebda054f355ad
Hamlet Abbaszade <hamletabbaszade0@gmail.com>
2025-12-04 23:50:05 +0400

    order

--- backend/warehouses_order_service/src/main/java/www/stock/az/exception/ExceptionConstants.java
+++ backend/warehouses_order_service/src/main/java/www/stock/az/exception/ExceptionConstants.java
@@ -5,5 +5,7 @@ public class ExceptionConstants {
     public static Integer SUCCCESS_CODE = 200;
     public static Integer NOT_FOUND_CODE = 404;
     public static Integer ERROR_CODE = 500;
-
+    public static Integer REQUEST_NULL = 400;
+    public static Integer REQUEST_SUCCESS = 401;
+    public static Integer REQUEST_FAIL = 403;
 }
--- backend/warehouses_order_service/src/main/java/www/stock/az/exception/MyException.java
+++ backend/warehouses_order_service/src/main/java/www/stock/az/exception/MyException.java
@@ -1,4 +1,12 @@
 package www.stock.az.exception;
 
-public class MyException {
+import java.io.IOException;
+
+public class MyException  extends RuntimeException{
+    private  Integer code;
+
+    public MyException(Integer code, String message) {
+        super(message);
+        this.code=code;
+    }
 }
